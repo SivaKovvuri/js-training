014ea3faefdefaef247ca36a9b131ae3dd83a08c
sivakovvuri <siva7050@yahoo.com>
2024-03-09 21:49:09 +0530

    12 and 13

--- exercises/12-random-bingo-card/script.js
+++ exercises/12-random-bingo-card/script.js
@@ -7,4 +7,15 @@ window.onload = createBingoCard;
 
 function createBingoCard() {
   // your code here
+  for (let index = 1; index <= 24; index++) {
+    let id = 'Square' + index
+    console.log(id)
+    document.getElementById(id).innerHTML = this.RandomValue(0, 75);
+  }
+
+
+}
+
+function RandomValue(min, max) {
+  return Math.ceil(Math.random() * (max - min) + min);
 }
--- exercises/13-show-prime-numbers/script.js
+++ exercises/13-show-prime-numbers/script.js
@@ -3,15 +3,30 @@
   numbers from 2 to that number.
  */
 function showPrimeNumbers(numberLimit) {
+  if(numberLimit<2){
+    return false;
+  }
+
   // your code goes here
-}
+  for (let index = 2; index <= numberLimit; index++) {
+    if(isPrimeNumber(index)){
+      console.log(index)
+    }
+    
+  }
+}           
 
 /*
   Write a function that takes a number and returns true if the number
   is prime, false otherwise.
  */
 function isPrimeNumber(_number) {
-  // your code goes here
+ for (let index = 2; index <= Math.sqrt(_number); index++) {
+    if(_number%index == 0){
+      return false;
+    }      
+ }
+  return true;
 }
 
 showPrimeNumbers(20); // 2, 3, 5, 7, 11, 13, 17, 19
